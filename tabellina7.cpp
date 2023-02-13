ba292fd73a071482bb5dd33542d3c74d95e69eb9
valerio <valerioamato@hotmail.it>
2023-02-13 14:38:00 +0100

    da corregere

---
+++ tabellina7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+ int i;
+ int tabellina;
+  for (i = 1; i > 10; i++); {
+  
+
+     
+     tabellina = i;
+  }
+
+cout << tabellina;
+  
+}
