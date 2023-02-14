6feec935c0f97a531a79fdad2231c1ecf9ec5eb5
valerio <valerioamato@hotmail.it>
2023-02-14 11:50:10 +0100

    funge!!!

--- ordinedec150300.cpp
+++ ordinedec150300.cpp
@@ -3,16 +3,18 @@
 #include <math.h>
 
 using namespace std;
-int main(){
- int i = 300; 
- int num = 0;
+int main()
+{
+    int i = 300;
+    int num = 0;
 
-    for(int i = 300;i >= 150; i--){
-        num = num + i;
-      if(num & 3 == 0){
-       cout << num;
-      }      
-      
-     
+    for (int i = 300; i >= 150; i--)
+    {                  // considero i numeri chiesti partendo da 300 a scendere verso 150
+        num = num + i; //
+        if (num % 3 == 0)
+        {
+            cout << i << endl;
+        }
+         
     }
 }
