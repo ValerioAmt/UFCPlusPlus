a1766763fe7a8ca5f4187dfddfeda3ddd8b6cc10
valerio <valerioamato@hotmail.it>
2023-02-14 15:12:53 +0100

    prova funzione tabellina

---
+++ funzionetabellina.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int i = 0;
+int tabellina = 0;
+int x = 0;
+
+stampaTabellina(x);{ 
+      for (int i = O; i <= 10; i++)
+      tabellina = i * x;
+      return = tabellina;
+      
+}
+int main()
+{
+       cout << "Inserisci un numero";
+       cin >> x;
+    stampaTabellina(5);
+    return tabellina;
+}
