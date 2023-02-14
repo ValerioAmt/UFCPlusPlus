d54a799134fbc04b44de675f33ddd7b34602d9b2
valerio <valerioamato@hotmail.it>
2023-02-14 10:45:34 +0100

    somma quarato gira ma non da risultato giusto

---
+++ sommaquadrato10numeri.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){ 
+int totale = 0;
+int x = 0;
+   
+     while (x<=10){
+      totale = totale + sqrt(x); 
+      x ++;
+     }
+     cout << totale;
+}
