04d2c2bb115050a6067f8bc3a0d876a6a40e75ac
valerio <valerioamato@hotmail.it>
2023-02-15 16:20:45 +0100

    corretto parola piu lunga

--- cisonoDoppie.cpp
+++ cisonoDoppie.cpp
@@ -5,7 +5,7 @@ using namespace std;
 string nome;
 bool ciSonoDoppie(){
    for(int i = 0;i < nome.lenght ;i ++)
-     if (nome[i] == nome[i+1]){
+     if (nome[i] == nome + 1){
          return true;
      }
      else {
@@ -18,5 +18,5 @@ bool ciSonoDoppie(){
 int main(){
     
      ciSonoDoppie();
-
+ return 0;
 }
--- parolaPiuLunga.cpp
+++ parolaPiuLunga.cpp
@@ -5,17 +5,16 @@ using namespace std;
 
 int main(){
 
-string parolaPiuLunga "";
+string parolaPiuLunga = "";
 string nuovaParola;
 
 for (int i = 0; i < 10; i++){
       cout << "inserisci una parola" << endl;
       cin >> nuovaParola;
       if(nuovaParola > parolaPiuLunga){
-          nuovaParola = parolaPiuLunga;
+          parolaPiuLunga = nuovaParola;
       }
 }
-      cout << "la parola inserità con piu caratteri è" << parolaPiuLunga;
-
-
+      cout << "la parola inserità con piu caratteri è " << parolaPiuLunga << "\n";
+        return 0;
 }
