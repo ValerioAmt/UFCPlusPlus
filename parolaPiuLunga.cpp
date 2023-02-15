a31462510f5ddb0ac6fc0631044444ecc1d1c5a6
valerio <valerioamato@hotmail.it>
2023-02-15 16:27:33 +0100

    parola lunga corretto

--- parolaPiuLunga.cpp
+++ parolaPiuLunga.cpp
@@ -3,18 +3,24 @@
 
 using namespace std;
 
-int main(){
-
 string parolaPiuLunga = "";
 string nuovaParola;
-
-for (int i = 0; i < 10; i++){
-      cout << "inserisci una parola" << endl;
-      cin >> nuovaParola;
-      if(nuovaParola > parolaPiuLunga){
-          parolaPiuLunga = nuovaParola;
+string trovaParolaLunga()
+{
+      for (int i = 0; i < 10; i++)
+      {
+            cout << "inserisci una parola" << endl;
+            cin >> nuovaParola;
+            if (nuovaParola > parolaPiuLunga)
+            {
+                  parolaPiuLunga = nuovaParola;
+            }
       }
-}
       cout << "la parola inserità con piu caratteri è " << parolaPiuLunga << "\n";
-        return 0;
-}z
+          return trovaParolaLunga;
+}
+
+int main(){
+
+     string trovaParolaLunga()
+}
