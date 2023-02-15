58a5b73c02ea7cd9889f2df515a7fb80005c852a
valerio <valerioamato@hotmail.it>
2023-02-15 16:03:00 +0100

    parola piu lunga

--- cisonodoppie.cpp
+++ cisonodoppie.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 using namespace std;
-
+string nome;
 bool ciSonoDoppie(){
    for(int i = 0;i < nome.lenght ;i ++)
      if (nome[i] == nome[i+1]){
@@ -16,7 +16,7 @@ bool ciSonoDoppie(){
 }
 
 int main(){
-    string nome;
+    
      ciSonoDoppie();
 
 }
